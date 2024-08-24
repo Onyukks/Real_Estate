3a037c7bbb28def32bb2e29aa728f15e4359e051
Onyeukwu <onyeukwuagbafo@gmail.com>
2024-08-24 11:55:23 +0100

    env

--- frontend/src/pages/NewPost/NewPost.jsx
+++ frontend/src/pages/NewPost/NewPost.jsx
@@ -197,8 +197,8 @@ const NewPost =()=> {
         ))}
       </div>
          <UploadWidget uwConfig={{
-          cloudName:process.env.VITE_CLOUD_NAME,
-          uploadPreset: process.env.VITE_CLOUD_PRESET,
+          cloudName:`${process.env.VITE_CLOUD_NAME}`,
+          uploadPreset:`${process.env.VITE_CLOUD_PRESET}`,
           multiple:true,
           folder:"residencyImages"
         }}
--- frontend/src/pages/ProfileUpdate/ProfileUpdate.jsx
+++ frontend/src/pages/ProfileUpdate/ProfileUpdate.jsx
@@ -62,8 +62,8 @@ const ProfileUpdate =()=> {
       <div className="sideContainer">
         <img src={avatar[0] || currentUser.avatar || '/noavatar.jpg'} alt="" className="avatar" />
         <UploadWidget uwConfig={{
-          cloudName:process.env.VITE_CLOUD_NAME,
-          uploadPreset: process.env.VITE_CLOUD_PRESET,
+          cloudName:`${process.env.VITE_CLOUD_NAME}`,
+          uploadPreset:`${process.env.VITE_CLOUD_PRESET}`,
           multiple:false,
           maxImageFileSize:2000000,
           folder:"avatars"
